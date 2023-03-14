cdd7aa18973d03b8ec2c23013b056fec70582fc3
maner92 <emrzrz1502@gmail.com>
2023-03-13 22:56:04 -0600

    Conexion API

---
+++ app/Http/Controllers/CharactersController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Inertia\Inertia;
+
+class CharactersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $characters = Http::get('https://valorant-api.com/v1/agents');
+
+        $jsonData = $characters->json();
+
+        return Inertia::render("Characters/Index", ["characters" => $jsonData]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Characters/Index.vue
@@ -0,0 +1,44 @@
+<template>
+    <AppLayout>
+        <div class="container">
+            <div class="row align-items-center justify-content-center">
+                <div v-for="character in characters.data" :key="character">
+                    <div v-if="character.isPlayableCharacter == true
+                    ">
+                        <div class="card mb-3" style="max-width: 540px;">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img :src=character.fullPortrait class="img-fluid rounded-start" alt="">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h1 class="card-title">{{ character.displayName }}</h1>
+                                        <p class="card-text">{{ character.description }}</p>
+                                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                            <button type="button" class="btn btn-outline-danger">See info</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AppLayout>
+</template>
+
+<script>
+import AppLayout from '@/Layouts/AppLayout.vue';
+export default {
+    props: {
+        characters: Object
+    },
+    components: {
+        AppLayout
+    }
+}
+</script>
+
+<style></style>
